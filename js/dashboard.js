9899a8691b022e4fb26c2382139f4fac54b27776
sandhyashiva2006-netizen <sandhyashiva2006@gmail.com>
2026-06-25 19:55:11 +0530

    update the files

--- js/dashboard.js
+++ js/dashboard.js
@@ -77,3 +77,105 @@ data.rating;
 
 loadAnalytics();
 
+async function loadMyNovels(){
+
+try{
+
+const response =
+await fetch(
+
+`${API}/api/writers/my-novels/${dashboardUser.id}`
+
+);
+
+const novels =
+await response.json();
+
+const body =
+document.getElementById(
+"novelsTableBody"
+);
+
+body.innerHTML="";
+
+if(novels.length===0){
+
+body.innerHTML=`
+
+<tr>
+
+<td colspan="7">
+
+No novels found
+
+</td>
+
+</tr>
+
+`;
+
+return;
+
+}
+
+novels.forEach(novel=>{
+
+body.innerHTML+=`
+
+<tr>
+
+<td>${novel.title}</td>
+
+<td>${novel.language}</td>
+
+<td>${novel.category}</td>
+
+<td>${novel.status}</td>
+
+<td>${novel.views}</td>
+
+<td>${novel.followers}</td>
+
+<td>
+
+<button
+class="edit-btn"
+onclick="editNovel(${novel.id})">
+
+Edit
+
+</button>
+
+<button
+class="edit-btn"
+onclick="manageChapters(${novel.id})">
+
+Chapters
+
+</button>
+
+<button
+class="delete-btn"
+onclick="deleteNovel(${novel.id})">
+
+Delete
+
+</button>
+
+</td>
+
+</tr>
+
+`;
+
+});
+
+}
+catch(err){
+
+console.log(err);
+
+}
+
+}
+
