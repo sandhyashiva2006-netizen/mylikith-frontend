5e5db8910304894f276506672a396ac01da97210
sandhyashiva2006-netizen <sandhyashiva2006@gmail.com>
2026-06-25 16:23:54 +0530

    update the files

--- js/profile.js
+++ js/profile.js
@@ -57,7 +57,7 @@ stats.comments;
 
 }
 
-loadStats();
+
 
 async function loadContinueReading(){
 
@@ -89,9 +89,13 @@ container.innerHTML = `
 
 <div class="continue-card">
 
+<div>
+
 <h3>${data.novel_title}</h3>
 
 <p>${data.chapter_title}</p>
+
+</div>
 
 <button
 onclick="window.location='reader.html?chapter=${data.chapter_id}'">
@@ -106,4 +110,211 @@ Continue Reading
 
 }
 
+
+
+async function loadHistory(){
+
+const response =
+await fetch(
+
+`${API}/api/profile/history/${user.id}`
+
+);
+
+const history =
+await response.json();
+
+const container =
+document.getElementById(
+"historyList"
+);
+
+container.innerHTML="";
+
+if(history.length===0){
+
+container.innerHTML=
+"No reading history.";
+
+return;
+
+}
+
+history.forEach(item=>{
+
+container.innerHTML+=`
+
+<div class="history-card">
+
+<div>
+
+<h3>${item.novel_title}</h3>
+
+<p>${item.chapter_title}</p>
+
+</div>
+
+<button
+onclick="window.location='reader.html?chapter=${item.chapter_id}'">
+
+Read Again
+
+</button>
+
+</div>
+
+`;
+
+});
+
+}
+
+
+
+async function loadReviews(){
+
+const response =
+await fetch(
+
+`${API}/api/profile/reviews/${user.id}`
+
+);
+
+const reviews =
+await response.json();
+
+const container =
+document.getElementById(
+"reviewsList"
+);
+
+container.innerHTML="";
+
+if(reviews.length===0){
+
+container.innerHTML=
+"No reviews yet.";
+
+return;
+
+}
+
+reviews.forEach(review=>{
+
+container.innerHTML+=`
+
+<div class="review-card">
+
+<div>
+
+<h3>
+
+<a href="novel.html?id=${review.novel_id}">
+
+${review.novel_title}
+
+</a>
+
+</h3>
+
+<p>
+
+⭐ ${review.rating}/5
+
+</p>
+
+<p>
+
+${review.review}
+
+</p>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+}
+
+
+
+async function loadComments(){
+
+const response =
+await fetch(
+
+`${API}/api/profile/comments/${user.id}`
+
+);
+
+const comments =
+await response.json();
+
+const container =
+document.getElementById(
+"commentsList"
+);
+
+container.innerHTML = "";
+
+if(comments.length===0){
+
+container.innerHTML =
+"No comments yet.";
+
+return;
+
+}
+
+comments.forEach(comment=>{
+
+container.innerHTML += `
+
+<div class="comment-card">
+
+<div>
+
+<h3>
+
+<a href="reader.html?chapter=${comment.chapter_id}">
+
+${comment.novel_title}
+
+</a>
+
+</h3>
+
+<p>
+
+<b>${comment.chapter_title}</b>
+
+</p>
+
+<p>
+
+${comment.comment}
+
+</p>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+}
+
+loadStats();
+
 loadContinueReading();
+
+loadHistory();
+
+loadReviews();
+
+loadComments();
