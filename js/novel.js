963faa36ce77fc7ea136cf93e2753990aaf01a76
sandhyashiva2006-netizen <sandhyashiva2006@gmail.com>
2026-06-27 22:32:27 +0530

    update the files

--- js/novel.js
+++ js/novel.js
@@ -43,6 +43,7 @@ style="width:100%;height:100%;object-fit:cover;border-radius:20px;">
 
 loadFollowers();
 loadRating();
+loadRelated();
 checkFollowStatus();
 
 if (!novel || !novel.id) {
@@ -605,6 +606,65 @@ alert("Added to Library.");
 }
 
 };
+
+async function loadRelated(){
+
+const response=await fetch(
+
+`${API}/api/novels`
+
+);
+
+const novels=await response.json();
+
+const container=
+
+document.getElementById("relatedNovels");
+
+container.innerHTML="";
+
+novels
+
+.filter(n=>n.id!==currentNovel.id)
+
+.filter(n=>n.category===currentNovel.category)
+
+.slice(0,4)
+
+.forEach(novel=>{
+
+container.innerHTML+=`
+
+<div
+class="related-card"
+onclick="location.href='novel.html?id=${novel.id}'">
+
+<img
+src="${novel.cover_url||'assets/images/default-cover.png'}">
+
+<div class="related-card-body">
+
+<h3>
+
+${novel.title}
+
+</h3>
+
+<p>
+
+${novel.category}
+
+</p>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+}
 
 loadNovel();
 loadChapters();
