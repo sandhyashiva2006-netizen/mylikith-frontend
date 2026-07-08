a7cdb0f8f3bbb8a2bc1e1dcd81358bd79b345926
sandhyashiva2006-netizen <sandhyashiva2006@gmail.com>
2026-07-09 00:52:39 +0530

    update

--- js/my-novels.js
+++ js/my-novels.js
@@ -42,56 +42,17 @@ grid.innerHTML += `
 
 <div class="novel-card">
 
-<img
-src="${novel.cover_url || 'assets/images/default-cover.png'}"
-alt="${novel.title}">
-
-<div class="novel-info">
-
-<div>
-
-<div style="display:flex;justify-content:space-between;align-items:center;gap:20px;">
-
 <h3>${novel.title}</h3>
 
-<span style="
-padding:8px 18px;
-border-radius:30px;
-background:${novel.status==="Completed"
-?"#059669"
-:novel.status==="Paused"
-?"#DC2626"
-:"#2563EB"};
-color:#fff;
-font-size:14px;
-font-weight:700;
-">
-
-${novel.status}
-
-</span>
-
-</div>
-
-<div class="novel-meta">
-
-<span>📚 ${novel.category}</span>
-
-<span>🌐 ${novel.language}</span>
-
-<span>👁 ${novel.views || 0}</span>
-
-<span>❤️ ${novel.followers || 0}</span>
-
-</div>
-
-<p class="novel-description">
-
-${novel.description || "No description added yet."}
-
+<p>
+${novel.category}
+•
+${novel.language}
 </p>
 
-</div>
+<p>
+${novel.status}
+</p>
 
 <div class="actions">
 
@@ -99,23 +60,15 @@ ${novel.description || "No description added yet."}
 class="edit-btn"
 onclick="editNovel(${novel.id})">
 
-✏ Edit Novel
+Edit
 
 </button>
 
 <button
-class="chapter-btn"
+class="edit-btn"
 onclick="manageChapters(${novel.id})">
 
-📖 Writer Studio
-
-</button>
-
-<button
-class="analytics-btn"
-onclick="location.href='writer-dashboard.html?novel=${novel.id}'">
-
-📊 Analytics
+Chapters
 
 </button>
 
@@ -123,11 +76,9 @@ onclick="location.href='writer-dashboard.html?novel=${novel.id}'">
 class="delete-btn"
 onclick="deleteNovel(${novel.id})">
 
-🗑 Delete
+Delete
 
 </button>
-
-</div>
 
 </div>
 
@@ -156,7 +107,7 @@ window.location =
 function manageChapters(id){
 
 window.location =
-`writer-studio.html?novel=${id}`;
+`my-chapters.html?novel=${id}`;
 
 }
 
